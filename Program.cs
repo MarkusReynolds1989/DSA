1ba2d4e234d80385d619d617334173c2e0e71dab
Markus <markusreynolds1989@gmail.com>
2020-06-30 21:04:58 -0500

    MergeSort

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using DSA.Sorting;
 
 namespace DSA
 {
@@ -9,6 +10,12 @@ namespace DSA
     {
         public static void Main(string[] args)
         {
+            int[] items = {2, 1, 6, 5, 3, 4, 11, 0, 22, 55};
+            var test = MergeSort.FnMerge(items);
+            foreach (var item in test)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
--- Sorting/MergeSort.cs
+++ Sorting/MergeSort.cs
@@ -2,30 +2,79 @@
 
 namespace DSA.Sorting
 {
-    public class MergeSort
+    public static class MergeSort
     {
         // Split Array
-        public int[] Merge(int[] array)
+        public static int[] FnMerge(int[] array)
         {
             // First slice goes up to half.
-            var half1 = array[..(array.Length/2)];
+            var half1 = array[..(array.Length / 2)];
             // Second slice goes from half to the end.
-            var half2 = array[(array.Length/2 + 1)..];
+            var half2 = array[(array.Length / 2)..];
             // Sort both of them.
-            var sorted1 = Sort(half1,0);
+            // Works from here.
+            var sorted1 = Sort(half1, 0);
             var sorted2 = Sort(half2, 0);
-            
-            // Merge them together.
+
+            // Do the merge.
+            return Merge(sorted1, sorted2);
         }
 
-        public int[] Sort(int[] array, int count)
+        public static int[] Merge(int[] array1
+            , int[] array2
+            , int lPos = 0
+            , int rPos = 0
+            , int count = 0
+            , int[] sorted = default
+        )
         {
-            int[] sorted = {};
-            if (count > array.Length)
+            sorted ??= new int[array1.Length + array2.Length];
+
+            // If there are no more elements in either array
+            if (count > array1.Length + array2.Length - 1)
             {
                 return sorted;
             }
-            
+
+            if (array1[lPos] < array2[rPos])
+            {
+                sorted[count] = array1[lPos];
+                if (lPos < array1.Length - 1)
+                {
+                    lPos += 1;
+                }
+            }
+            else
+            {
+                sorted[count] = array2[rPos];
+                if (rPos < array2.Length - 1)
+                {
+                    rPos += 1;
+                }
+            }
+
+            return Merge(array1, array2, lPos, rPos, count + 1, sorted);
+        }
+
+        // Destructive sort.
+        public static int[] Sort(int[] array, int count)
+        {
+            if (count >= array.Length - 1)
+            {
+                return array;
+            }
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    var temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+
+            return Sort(array, count + 1);
         }
     }
 }
